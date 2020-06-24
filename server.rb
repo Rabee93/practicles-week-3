87458e4311f84f21bbd289997eea9bfae97fe242
Rabee93 <rabei_mghrabi@yahoo.com>
2020-06-24 12:58:32 +0100

    solved exercise 2 part 2

---
+++ server.rb
@@ -0,0 +1,15 @@
+require 'socket'
+
+server = TCPServer.new(2345)
+
+socket = server.accept
+while true do
+socket.puts "What do you say?"
+
+they_said = socket.gets.chomp
+
+break if they_said == "quit"
+
+socket.puts "You said: #{they_said}. Goodbye!"
+end 
+socket.close
